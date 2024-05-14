79e9e4c1109d03abdfa1aac5d0a66aa82fa9f4e4
zzzzzzzoou <zzzzzzzoou@gmail.com>
2024-05-14 17:31:55 +0800

    proxy config modify

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        plugins: [react()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
         },
-    },
-    server: {
-        proxy: {
-          '/localapi': {
-            target: "https://qianji-api.nfthook.xyz",
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/localapi/, '')
-          }
+        server: {
+            proxy: {
+              [env.VITE_GLOB_API_URL]: {
+                target: env.VITE_PROXY_URL,
+                changeOrigin: true,
+                // rewrite: (path) => path.replace(/^\/localapi/, ''),
+                rewrite: (path) => path.replace(new RegExp(`^${env.VITE_GLOB_API_URL.replace(/\//g, '\\/')}`), ''),
+              }
+            }
         }
     }
 });
