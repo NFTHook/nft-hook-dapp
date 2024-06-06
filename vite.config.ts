a2d8f21e39829111a96ec7e85d5d26b860ba4365
zzzzzzzoou <zzzzzzzoou@gmail.com>
2024-06-06 17:22:10 +0800

    Setting meta

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
+    const env = loadEnv(mode, process.cwd(), "");
     return {
-        plugins: [react()],
+        plugins: [
+            react(),
+            createHtmlPlugin({
+                inject: {
+                    data: {
+                        title: 'NFTHook',
+                        description: 'Empowering Every Transaction: NFTs for All on Layer 2',
+                    },
+                },
+            }),
+        ],
         resolve: {
             alias: {
                 "@": path.resolve(__dirname, "./src"),
@@ -14,12 +25,12 @@ export default defineConfig(({ mode }) => {
         },
         server: {
             proxy: {
-              [env.VITE_GLOB_API_URL]: {
-                target: env.VITE_PROXY_URL,
-                changeOrigin: true,
-                rewrite: (path) => path.replace(new RegExp(`^${env.VITE_GLOB_API_URL.replace(/\//g, '\\/')}`), ''),
-              }
-            }
-        }
-    }
+                [env.VITE_GLOB_API_URL]: {
+                    target: env.VITE_PROXY_URL,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(new RegExp(`^${env.VITE_GLOB_API_URL.replace(/\//g, "\\/")}`), ""),
+                },
+            },
+        },
+    };
 });
